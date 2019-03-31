fe6d48c2455db85d80e8664fc16b33586c3fc2b9
m5r <mokht@rmi.al>
2019-03-31 23:22:57 +0200

    * rename app * local notification helper

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,30 @@
 import { useEffect } from 'react';
+import { LocalNotification, LocalNotificationScheduleResult, Plugins } from '@capacitor/core';
 
-export const useBackButton = (onBackButtonPressed: () => void) => {
+export function useBackButton(onBackButtonPressed: VoidFunction) {
 	useEffect(() => {
 		document.addEventListener('backbutton', onBackButtonPressed, false);
 
 		return () => document.removeEventListener('backbutton', onBackButtonPressed, false);
 	}, []);
-};
+}
+
+export async function registerNotification(notification: LocalNotification): Promise<LocalNotificationScheduleResult | undefined> {
+	if (Plugins.LocalNotifications) {
+		if (await Plugins.LocalNotifications.areEnabled()) {
+			/*const notification: LocalNotification = {
+				title: 'Notification from Notedown',
+				body: 'This is the notification body',
+				id: 0,
+				schedule: {
+					at: new Date(Date.now() + 5000),
+				},
+			};*/
+			const options = {
+				notifications: [notification],
+			};
+
+			return Plugins.LocalNotifications.schedule(options);
+		}
+	}
+}
